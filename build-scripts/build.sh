2cf1577c2540039acab4082500bda178c2255b1f
Alessandro Rinaldi <ale.rinaldi@criptalia.it>
2019-07-16 17:45:27 +0200

    Move build in separate script

---
+++ build-scripts/build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+type=$1
+
+if [ "$type" != "client" ] && [ "$type" != "server" ]; then
+    echo "Usage: $0 [client|server]"
+    exit 1
+fi
+
+if [ "$TRAVIS_COMMIT" != "" ]; then
+    commit=$(echo "$TRAVIS_COMMIT" | head -c 7);
+    branch="$TRAVIS_BRANCH";
+    version="$commit ($branch)"
+elif [ $(which git) != "" ]; then
+    commit=$(git rev-parse HEAD | head -c 7);
+    branch=$(git rev-parse --abbrev-ref HEAD);
+    version="$commit ($branch) - UNOFFICIAL BUILD"
+else
+   version="UNOFFICIAL BUILD"
+fi
+
+echo "Building $type for $GOOS $GOARCH - ver. $version"
+go build -ldflags "-X 'main.Version=$version'" -o dist/$GOOS/$GOARCH/engarde-$type ./cmd/engarde-$type
